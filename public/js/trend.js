2d6f841a2b81300d02c92454443483bbc6812972
Raymond Nucuta <rnucuta@gmail.com>
2018-12-29 16:18:16 -0700

    trending

--- public/js/trend.js
+++ public/js/trend.js
@@ -1,34 +1,40 @@
-var maxVideos = 5;
-   $(document).ready(function(){
-  $.get(
-    "https://www.googleapis.com/youtube/v3/videos",{
-      part: 'snippet',
-      chart: 'mostPopular',
-      kind: 'youtube#videoListResponse',
-      maxResults: maxVideos,
-      regionCode: 'IN',
-      key: 'AIzaSyC1U3lIrDYfK9slvPCFlbEnAx3IsmMNN10'},
-      function(data){
-        var output;
-        $.each(data.items, function(i, item){
-          console.log(item);
-          videTitle = item.snippet.title;
-                description = item.snippet.description;
-                thumb = item.snippet.thumbnails.high.url;
-                channelTitle = item.snippet.channelTitle;
-                videoDate = item.snippet.publishedAt;
-                Catagoryid = item.snippet.categoryId;
-                cID = item.snippet.channelId;
-          output = '<div class="maindiv"><div>' +
-                        '<a data-fancybox-type="iframe" class="fancyboxIframe" href="watch.php?v=' + vidId + '" target="_blank" ><img src="' + thumb + '" class="img-responsive thumbnail" ></a>' +
-                        '</div>' +
-                        '<div class="input-group col-md-6">' +
-                            '<h3 class="Vtitle"><a data-fancybox-type="iframe" class="fancyboxIframe" href="watch.php?v=' + vidId + '" target="_blank">' + videTitle + '</a></h3>'+
-                        '</div><div  id="cTitle"><a href="https://www.youtube.com/channel/'+cID+'" target="_blank">'+channelTitle+'</a></div></div>' +
-                    '<div class="clearfix"></div>';
-          $('#trending').append(output);
-        })
 
-      }
-    );
-}); 
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.videos.list({
+            part: "contentDetails",
+            chart="mostPopular",
+            regionCode="IN",
+            maxResults: 5,
+            order: "relevance",
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("tpl/item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9/16);
+}
+
+function init() {
+    gapi.client.setApiKey("AIzaSyC1U3lIrDYfK9slvPCFlbEnAx3IsmMNN10");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
