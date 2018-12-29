6d3f0e61bbd65cc2f0ef854e7a031546daaf0ddb
Raymond Nucuta <rnucuta@gmail.com>
2018-12-29 12:50:17 -0700

    verify

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ $(function() {
             part: "snippet",
             type: "video",
             q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 3,
+            maxResults: 5,
             order: "relevance",
        }); 
        // execute the request
